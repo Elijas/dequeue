eda1359397f844e31187d2132031cd40df7b5d62
Elijas <master.elijas@gmail.com>
2018-04-07 21:28:00 +0300

    Add REPL UI

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    while (true) {
+        std::cout << "Current deck: empty" << std::endl;
+        std::cout << "Instructions: 'a' to add, 'd' to delete, 'x' to exit. "
+                  << "Use capital letter to use front of deque." << std::endl;
+        char command;
+        std::cin >> command;
+        if (std::tolower(command) == 'x')
+            break;
+        else if (std::tolower(command) == 'd')
+            std::cout << "D" << std::endl;
+        else if (std::tolower(command) == 'a') {
+            std::cout << "A" << std::endl;
+        }
+        else {
+            std::cout << "Incorrect input, no operation is performed" << std::endl;
+        }
+    }
     return 0;
 }
