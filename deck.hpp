7493a8e0ee0236c05a714690bf6ac61268904a2e
Elijas <master.elijas@gmail.com>
2018-04-08 01:19:43 +0300

    Add push back number

--- deck.hpp
+++ deck.hpp
@@ -7,11 +7,53 @@ struct Node {
     Node *next = nullptr;
 
     void connectNext(Node *next);
+
+    Node(double value) {
+        this->data = value;
+    }
 };
 
 void Node::connectNext(Node *next) {
     this->next = next;
     next->previous = this;
+}
+
+struct Deck {
+    Node *first = nullptr;
+    Node *last = nullptr;
+public:
+    bool isEmpty();
+
+    double peekFirst();
+
+    double peekLast();
+
+    void pushBack(double);
+};
+
+bool Deck::isEmpty() {
+    return this->first == nullptr && this->last == nullptr;
+}
+
+double Deck::peekFirst() {
+    return this->first->data;
+}
+
+double Deck::peekLast() {
+    return this->last->data;
+}
+
+void Deck::pushBack(double value) {
+    int a=0;
+    Node* node = new Node(value);
+    int b=1;
+    if (this->isEmpty()) {
+        this->first = node;
+        this->last = node;
+        return;
+    }
+    this->last->connectNext(node);
+    this->last = node;
 }
 
 #endif //Y1S2_ALG_SD2_DECK_H
--- deck_tests.cpp
+++ deck_tests.cpp
@@ -4,13 +4,43 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "lib/catch.hpp"
 #include "deck.hpp"
+#define ASSERT(x) REQUIRE((x)) // Hot fix for bug when test library messes up pointers of the object tested, particulary "1 == d.peekLast()", when function implemented as "return this->first->data"
+
 
 TEST_CASE("Nodes can be linked") {
-    Node a{.data=1};
-    Node b{.data=2};
+    Node a(1);
+    Node b(2);
 
     a.connectNext(&b);
 
-    REQUIRE(a.next->data == 2);
-    REQUIRE(b.previous->data == 1);
+    ASSERT(a.next->data == 2);
+    ASSERT(b.previous->data == 1);
+}
+
+TEST_CASE("Deck is initialized empty") {
+    //-
+
+    Deck d;
+
+    ASSERT(d.isEmpty());
+}
+
+TEST_CASE("Deck receives the first value correctly") {
+    Deck d;
+
+    d.pushBack(1);
+
+    ASSERT(!d.isEmpty());
+    ASSERT(d.peekFirst() == 1);
+    ASSERT(d.peekLast() == 1);
+}
+
+TEST_CASE("Deck receives the second value correctly") {
+    Deck d;
+    d.pushBack(1);
+
+    d.pushBack(2);
+
+    ASSERT(d.peekFirst() == 1);
+    ASSERT(d.peekLast() == 2);
 }
